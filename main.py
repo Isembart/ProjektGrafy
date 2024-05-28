5397157823a554b1110fb3742200bef73970069f
szala-jakub <kubasz2002@gmail.com>
2024-05-28 17:19:07 +0200

    added graph mapping file

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ A = [
     [0, 0, 3, 0]
 ]
 
+# funkcja do wyświetlania macierzy
+
 def printMatrix(A):
     for i in range(len(A)):
         print(A[i], sep=' ')
     print()
+
+
+# funkcja wykonująca algorytm floyda-warshalla 
+# przyjmuje macierz na wejściu macierz sądziedztwa
+# zwraca dwie macierze: pierwsza dlugość najkrótszej ścieżki z wierzchołki i (wiersz) do wierdzhołku j (kolumna)
+# druga: zwraca poprzedni wierzchołek
 
 def test(W):
     Distance = [[None for i in range(len(W))] for j in range(len(W))]
